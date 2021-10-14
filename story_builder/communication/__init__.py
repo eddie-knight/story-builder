c403f15a455688262a4dab808853701b31178f1b
Aaron Knight <knightaaron44@gmail.com>
2021-10-13 19:26:56 -0500

    added story line develepment

--- story_builder/communication/__init__.py
+++ story_builder/communication/__init__.py
@@ -31,3 +31,39 @@ turn a critical eye to our progress, and find some
 optimizations that we might otherwise miss.
 
 """
+
+""" FOREST:   You find sprites that are being beseiged by packs of 
+angry wolves that have been driven from their home.  The sprites in turn are just trying to get to the Caves in 
+order to save a sprite maiden that has been taken hostage in the 
+dungeon hidden below.  If you help the sprites they will give you a 
+HUNTING KNIFE to fight with.  Every pack of wolves you defeat they will
+craft you armor from their hides.  If you defeat all the packs 
+(5: Breast plate, Pants, Boots, Helmet, Gloves) you will have a full
+set of ARMOR and the boost it gives as well as be given a MACE.
+
+Caves: You will fight lesser skeletons that will earn you HEALTH POTIONS
+and in the LargeCavern you will fight a boss level skeleton that will 
+earn you a LONG BONE SWORD. As well as the story and why the hostage 
+was kidnapped (She was hired to be a guide to the dungeon, but was used 
+as bait and left for dead when the Adventurer found what he was looking for.)
+
+Coast:  You will fight hidden bandits that will earn you special RINGS. 
+at the end you will find and fight the Lead Bandit and his pet bird. You will 
+also learn why he stole the artifact, and that he gave it to 'some-hooded guy
+with a raspy voice'. You just missed him.  He went into the plains) but since
+it would be bad for business to rat out customers, the Bandit will have to 
+kill you anyway. He earns you a NECKLACE OF SWIFT MOVEMENT.
+
+Plains: Here you will fight 3 large, undead beasts.
+    Bear: earns you a BELT OF STRENGTH.
+    Cougar: earns you BOOTS OF STEALTH.
+    Python: earns you ANTIDOTE x 4 and POWDER OF STONE x 4
+
+Temple: Here you will fight off Slime monsters. (applying POWDER OF STONE to your
+weapons makes them solid enough to take damage from weapons.
+They will earn you HEALTH or POWDER or ANTIDOTE (antidote to their slime).
+in the Sanctum you will fight a powerful Large Slime.  He will earn you
+a RING OF SPELL REJECTION.   After the slime, a Lych is revealed, holding the Artifact.
+if you beat him, you win the artifact and the loot you have retained to this point. All of
+which can be sold or used in the REAL GAME (?), and the Arifact definitly is worth the time
+spent in our little world. What do you think?"""
